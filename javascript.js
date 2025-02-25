6cdc4b6f6291bca78f3572eb6bfd35712859a018
Selullie <142448746+Selullie@users.noreply.github.com>
2025-02-25 23:27:46 +0000

    Implemented running score & victory/defeat lines

--- javascript.js
+++ javascript.js
@@ -6,6 +6,7 @@ const winloseDiv = document.querySelector("#winlose");
 const scoreDiv = document.querySelector("#score");
 const playerScoreDiv = document.querySelector("#playerScore");
 const computerScoreDiv = document.querySelector("#computerScore");
+const winReadoutDiv = document.querySelector("#winReadout");
 let playerScore = 0;
 let computerScore = 0;
 let winner = 1;
@@ -26,9 +27,11 @@ function playRound(playerSelection, computerSelection) {
       } else if (computerChoice === "paper") {
         winner = 2;
         winloseDiv.textContent = "You lose!";
+        computerScore++;
       } else {
         winner = 1;
         winloseDiv.textContent = "You win!";
+        playerScore++;
       }
       break;
     case "paper":
@@ -38,9 +41,11 @@ function playRound(playerSelection, computerSelection) {
       } else if (computerChoice === "scissors") {
         winner = 2;
         winloseDiv.textContent = "You lose!";
+        computerScore++;
       } else {
         winner = 1;
         winloseDiv.textContent = "You win!";
+        playerScore++;
       }
       break;
     case "scissors":
@@ -50,9 +55,11 @@ function playRound(playerSelection, computerSelection) {
       } else if (computerChoice === "rock") {
         winner = 2;
         winloseDiv.textContent = "You lose!";
+        computerScore++;
       } else {
         winner = 1;
         winloseDiv.textContent = "You win!";
+        playerScore++;
       }
       break;
   }
@@ -61,20 +68,72 @@ function playRound(playerSelection, computerSelection) {
 
 rockBtn.addEventListener("click", function (e) {
   let computerSelection = getComputerChoice();
+  winReadoutDiv.textContent = "";
   playRound("rock", computerSelection);
   if (winner === 1) {
     playerScoreDiv.textContent = "Player Score: " + playerScore;
   } else if (winner === 2) {
     computerScoreDiv.textContent = "Computer Score: " + computerScore;
   }
+  if (playerScore === 5) {
+    winReadoutDiv.textContent = "You're the winner!";
+    playerScoreDiv.textContent = "Player Score: 0";
+    computerScoreDiv.textContent = "Computer Score: 0";
+    playerScore = 0;
+    computerScore = 0;
+  } else if (computerScore === 5) {
+    winReadoutDiv.textContent = "You lose this time!";
+    playerScoreDiv.textContent = "Player Score: 0";
+    computerScoreDiv.textContent = "Computer Score: 0";
+    playerScore = 0;
+    computerScore = 0;
+  }
 });
 
 paperBtn.addEventListener("click", function (e) {
   let computerSelection = getComputerChoice();
+  winReadoutDiv.textContent = "";
   playRound("paper", computerSelection);
+  if (winner === 1) {
+    playerScoreDiv.textContent = "Player Score: " + playerScore;
+  } else if (winner === 2) {
+    computerScoreDiv.textContent = "Computer Score: " + computerScore;
+  }
+  if (playerScore === 5) {
+    winReadoutDiv.textContent = "You're the winner!";
+    playerScoreDiv.textContent = "Player Score: 0";
+    computerScoreDiv.textContent = "Computer Score: 0";
+    playerScore = 0;
+    computerScore = 0;
+  } else if (computerScore === 5) {
+    winReadoutDiv.textContent = "You lose this time!";
+    playerScoreDiv.textContent = "Player Score: 0";
+    computerScoreDiv.textContent = "Computer Score: 0";
+    playerScore = 0;
+    computerScore = 0;
+  }
 });
 
 scissorsBtn.addEventListener("click", function (e) {
   let computerSelection = getComputerChoice();
+  winReadoutDiv.textContent = "";
   playRound("scissors", computerSelection);
+  if (winner === 1) {
+    playerScoreDiv.textContent = "Player Score: " + playerScore;
+  } else if (winner === 2) {
+    computerScoreDiv.textContent = "Computer Score: " + computerScore;
+  }
+  if (playerScore === 5) {
+    winReadoutDiv.textContent = "You're the winner!";
+    playerScoreDiv.textContent = "Player Score: 0";
+    computerScoreDiv.textContent = "Computer Score: 0";
+    playerScore = 0;
+    computerScore = 0;
+  } else if (computerScore === 5) {
+    winReadoutDiv.textContent = "You lose this time!";
+    playerScoreDiv.textContent = "Player Score: 0";
+    computerScoreDiv.textContent = "Computer Score: 0";
+    playerScore = 0;
+    computerScore = 0;
+  }
 });
